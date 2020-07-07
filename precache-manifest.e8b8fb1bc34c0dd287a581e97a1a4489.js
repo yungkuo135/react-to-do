ab93c8fce08be91ce78f57fcf53246adeccc9863
yung <kuotkt@gmail.com>
2020-07-07 12:50:38 +0800

    Updates

---
+++ precache-manifest.e8b8fb1bc34c0dd287a581e97a1a4489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b301d82e22c09f889de8f85ccc0d8bef",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "39a6175ff89434530499",
+    "url": "/react-to-do/static/css/main.6493af08.chunk.css"
+  },
+  {
+    "revision": "7d82232ae89972056d00",
+    "url": "/react-to-do/static/js/2.5b9f2843.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-to-do/static/js/2.5b9f2843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a6175ff89434530499",
+    "url": "/react-to-do/static/js/main.f57df6a8.chunk.js"
+  },
+  {
+    "revision": "937a9eca39302de3b5e8",
+    "url": "/react-to-do/static/js/runtime-main.7927655d.js"
+  }
+]);
